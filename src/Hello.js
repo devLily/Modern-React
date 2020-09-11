7395ce994adbacc0fdc95fd7db4acc241e8c6cee
glorroll <62908757+glorroll@users.noreply.github.com>
2020-09-12 02:00:58 +0900

    컴포넌트

---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Hello() {
+    return <div>안녕하세요</div>//jsx
+}// 컴포넌트 이름은 대문자로 입력한다
+
+export default Hello; 
+// Hello라는 컴포넌트를 만들어서 내보내 주겠다 라는 의미
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')//페이지에서 아이디가 루트인 것을 찾아 리액트 앱을 안에 넣겠다, 루트는 퍼블릭 내 인덱스 에치티엠엘 안에 잇음
 );
 
 // If you want your app to work offline and load faster, you can change
