b21dfeaded34aa1cd59d33ba49ffd4f64f326402
glorroll <62908757+glorroll@users.noreply.github.com>
2020-09-13 01:17:33 +0900

    jsx
    style 적용
    주석 작성 etc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
   return (
-    <div>
-      <Hello />
-    </div>
+    <>
+    {/*주석 작성하기*/}
+      <Hello 
+      // 이런식으로 작성하는 주석은 화면에 나타나지 않지롱
+      />
+      <div style ={style}>{name}</div>
+      <div className="gray-box"></div>
+      </>
   );
 }
-
 export default App;
